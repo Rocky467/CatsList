78ae1ead293ed3e28fbb4f7368d741595d3cf395
Rocky467 <chaudharirakesh467@gmail.com>
2022-01-09 18:25:37 +0530

    updates

--- app/src/main/java/com/google/assign/ui/ListViewModel.kt
+++ app/src/main/java/com/google/assign/ui/ListViewModel.kt
@@ -1,10 +1,11 @@
 package com.google.assign.ui
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.*
-import com.google.assign.model.User
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.cachedIn
+import androidx.paging.liveData
 import com.google.assign.network.ApiService
 import com.google.assign.network.RemoteDataSource
 
@@ -13,14 +14,5 @@ class ListViewModel(private val apiService: ApiService) : ViewModel() {
     val usersList = Pager(PagingConfig(pageSize = 10)) {
         RemoteDataSource(apiService)
     }.liveData.cachedIn(viewModelScope)
-
-    //  -- OR--
-
-    fun getUsers(): LiveData<PagingData<User>> {
-        return Pager(
-            config = PagingConfig(pageSize = 10),
-            pagingSourceFactory = { RemoteDataSource(apiService) }
-        ).liveData
-    }
 
 }
