b4d618751ac601d653acd469c1b25f935a1817ce
Rocky467 <chaudharirakesh467@gmail.com>
2022-01-09 17:33:20 +0530

    font added

--- app/src/main/java/com/google/assign/ui/list/ListAdapter.kt
+++ app/src/main/java/com/google/assign/ui/list/ListAdapter.kt
@@ -15,17 +15,15 @@ import com.google.assign.databinding.ItemLayoutBinding
 import com.google.assign.model.User
 
 class ListAdapter(private val userInterface: UserInterface) :
-    PagingDataAdapter<User, RecyclerView.ViewHolder>(DiffUtil) {
+    PagingDataAdapter<User, ListAdapter.UserViewHolder>(DiffUtil) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
         val binding = ItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return UserViewHolder(binding, userInterface)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        getItem(position)?.let {
-            if (holder is UserViewHolder) holder.bindData(it)
-        }
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        getItem(position)?.let { holder.bind(it) }
     }
 
     class UserViewHolder(
@@ -33,7 +31,7 @@ class ListAdapter(private val userInterface: UserInterface) :
         private val userInterface: UserInterface
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bindData(user: User) {
+        fun bind(user: User) {
             binding.apply {
                 this.user = user
             }
@@ -42,8 +40,7 @@ class ListAdapter(private val userInterface: UserInterface) :
                 userInterface.userClick(user)
             }
 
-            Glide.with(binding.root.context)
-                .load(user.avatar)
+            Glide.with(binding.root.context).load(user.avatar)
                 .diskCacheStrategy(DiskCacheStrategy.ALL)
                 .into(binding.image)
         }
@@ -62,7 +59,6 @@ val DiffUtil = object : DiffUtil.ItemCallback<User>() {
     override fun areItemsTheSame(oldItem: User, newItem: User): Boolean =
         oldItem.first_name == newItem.first_name
 
-    @SuppressLint("DiffUtilEquals")
     override fun areContentsTheSame(oldItem: User, newItem: User): Boolean =
         newItem == oldItem
 }
