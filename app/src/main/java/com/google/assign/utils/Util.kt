734e5cc999fc1ff27be77909d49102cba380c0d0
Rocky467 <chaudharirakesh467@gmail.com>
2024-07-24 12:03:28 +0530

    redundant files removed

--- app/src/main/java/com/google/assign/utils/Util.kt
+++ app/src/main/java/com/google/assign/utils/Util.kt
@@ -11,32 +11,21 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.google.assign.R
 
-fun log(tag: String, msg: Any?) {
-    Log.d(tag, "$msg")
-}
+fun log(tag: String, msg: Any?) = Log.d(tag, "$msg")
 
 @Suppress("unused")
-fun delays(millis: Long, function: () -> Unit) {
+fun delays(millis: Long, function: () -> Unit) =
     Handler(Looper.getMainLooper()).postDelayed({
         function()
     }, millis)
-}
 
-@BindingAdapter("android:loadUrl")
-fun loadUrl(imageView: ImageView, url: String?) {
-    url?.let {
-        imageView.load(it)
-    }
-}
-
-fun ImageView.load(url: String) {
-    Glide.with(context)
-        .load(url)
-        .placeholder(R.drawable.default_placeholder)
-        .centerCrop()
-        .diskCacheStrategy(DiskCacheStrategy.ALL)
-        .into(this)
-}
+@BindingAdapter("loadUrl")
+fun ImageView.loadUrl(url: String) = Glide.with(context)
+    .load(url)
+    .placeholder(R.drawable.default_placeholder)
+    .centerCrop()
+    .diskCacheStrategy(DiskCacheStrategy.ALL)
+    .into(this)
 
 fun <T : Any> diffUtil(
     areItemsTheSame: (oldItem: T, newItem: T) -> Boolean
